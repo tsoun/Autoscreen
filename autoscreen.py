225b12aea2c9841312a01e51d1f27036b0cb86c4
tsoun <=>
2020-11-19 21:42:04 +0200

    Update numbering and dirs

--- autoscreen.py
+++ autoscreen.py
@@ -1,12 +1,22 @@
+from os import mkdir
 import pyautogui
 import datetime
 from time import sleep
 from PIL import Image, ImageChops, ImageStat
 from pyautogui import failSafeCheck
-from os import remove
+import os
 
-def take_screen():
-    i = 10000
+def take_screen(cpath):
+    os.chdir(cpath)
+    npath = "Screens/" + str(datetime.datetime.today().strftime('%Y-%m-%d'))
+    if not os.path.exists(npath):
+        os.mkdir(npath)
+        os.chdir(npath)
+        i = 1
+    else:
+        os.chdir(npath)
+        i = len([name for name in os.listdir('.') if os.path.isfile(name)])
+
     while True:
         sleep(1)
         pyautogui.screenshot("screen" + str(i) + ".jpeg")
@@ -19,16 +29,19 @@ def take_screen():
             diff = ImageChops.difference(im1, im2)
             stat = ImageStat.Stat(diff)
             diff_ratio = sum(stat.mean) / len(stat.mean) * 255
-            if diff_ratio < 500:
-                remove("screen" + str(i-1) + ".jpeg")
-            print(diff_ratio)
+            if diff_ratio < 800:
+                os.remove("screen" + str(i-1) + ".jpeg")
+            print("Screen " + str(i) + " " + str(diff_ratio))
         i += 1
         if i == 600:
             break
 
 def main():
-    take_screen()
+    path = os.getcwd()
+    for i in range(0,5):
+        print("Screenshots begin in " + str(5-i) + "...")
+        sleep(1)
+    take_screen(path)
 
 if __name__ == "__main__":
-    sleep(5)
     main()
