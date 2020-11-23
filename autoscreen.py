22034633c6b535e62d7c3cd15b56c492dc0d1c9d
tsoun <=>
2020-11-23 23:03:12 +0200

    Improve rate and add exe

--- autoscreen.py
+++ autoscreen.py
@@ -1,43 +1,64 @@
-from os import mkdir
 import pyautogui
 import datetime
 from time import sleep
 from PIL import Image, ImageChops, ImageStat
-from pyautogui import failSafeCheck
 import os
 
 def take_screen(cpath):
+    lim = 0
     os.chdir(cpath)
     npath = "Screens/" + str(datetime.datetime.today().strftime('%Y-%m-%d'))
     if not os.path.exists(npath):
-        os.mkdir(npath)
+        os.makedirs(npath)
         os.chdir(npath)
         i = 1
     else:
         os.chdir(npath)
-        i = len([name for name in os.listdir('.') if os.path.isfile(name)])
+        i = 10000 
 
+    im1 = pyautogui.screenshot()
+    sleep(0.5)
+    im2 = pyautogui.screenshot()
+    
     while True:
         sleep(1)
-        pyautogui.screenshot("screen" + str(i) + ".jpeg")
+        try:
+            im1 = im2
+        except FileNotFoundError:
+            i += 1
+            continue
+        im2 = pyautogui.screenshot()
         if i > 1:
-            try:
-                im1 = Image.open("screen" + str(i-1) + ".jpeg")
-                im2 = Image.open("screen" + str(i) + ".jpeg")
-            except FileNotFoundError:
-                continue
             diff = ImageChops.difference(im1, im2)
             stat = ImageStat.Stat(diff)
             diff_ratio = sum(stat.mean) / len(stat.mean) * 255
-            if diff_ratio < 800:
-                os.remove("screen" + str(i-1) + ".jpeg")
+            if diff_ratio < 700:
+                print("Thrown in the garbage screen" + str(i-1) + ".jpeg due to insignificant change.")
+            else:
+                im2.save("screen" + str(i) + ".jpeg")
+                print("Saved screen" + str(i) + ".jpeg.")
             print("Screen " + str(i) + " " + str(diff_ratio))
         i += 1
-        if i == 600:
+        lim += 1
+        if lim == 7500:
             break
 
 def main():
     path = os.getcwd()
+
+    print("""
+
+ ______     __  __     ______   ______     ______     ______     ______     ______     ______     __   __    
+/\  __ \   /\ \/\ \   /\__  _\ /\  __ \   /\  ___\   /\  ___\   /\  == \   /\  ___\   /\  ___\   /\ "-.\ \   
+\ \  __ \  \ \ \_\ \  \/_/\ \/ \ \ \/\ \  \ \___  \  \ \ \____  \ \  __<   \ \  __\   \ \  __\   \ \ \-.  \  
+ \ \_\ \_\  \ \_____\    \ \_\  \ \_____\  \/\_____\  \ \_____\  \ \_\ \_\  \ \_____\  \ \_____\  \ \_\\"\_\ 
+  \/_/\/_/   \/_____/     \/_/   \/_____/   \/_____/   \/_____/   \/_/ /_/   \/_____/   \/_____/   \/_/ \/_/ by tsoun
+                                                                                                             
+""")
+    sleep(2)
+    print("You will find the screenshots at the sudirectory .../autoscreen/Screens/. The garbage screenshots will be thrown\n*aggressively* in my virtual trash can. Have fun.")
+    input("Press any key to start screenshoting...\n")
+    
     for i in range(0,5):
         print("Screenshots begin in " + str(5-i) + "...")
         sleep(1)
